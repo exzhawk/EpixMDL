f10419deac8290be8bc9929e576caf8d0aa9b1c0
Epix <git@exz.me>
2016-10-05 14:28:33 +0800

    add mdl link

--- header.php
+++ header.php
@@ -16,6 +16,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://code.getmdl.io/1.2.1/material.indigo-pink.min.css">
+<script defer src="https://code.getmdl.io/1.2.1/material.min.js"></script>
 <?php wp_head(); ?>
 </head>
 
