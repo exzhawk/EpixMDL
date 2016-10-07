cc0468037440e2d249fca2f35a734b04e182b781
Epix <git@exz.me>
2016-10-07 19:44:39 +0800

    stop using too much printf

--- template-parts/content.php
+++ template-parts/content.php
@@ -9,51 +9,55 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('mdl-grid mdl-grid--no-spacing mdl-shadow--2dp'); ?>>
-    <?php if (!is_single()): ?>
-        <header class="">
-            <?php the_post_thumbnail('w320h320'); ?>
-        </header>
-    <?php endif; ?>
-    <div class="mdl-card">
-        <div class="mdl-card__title entry-header">
-            <?php the_title('<h2 class="mdl-card__title-text entry-title">', '</h2>') ?>
-        </div>
-        <?php $post_tags = get_the_tags();
-        if ($post_tags) {
-            ?>
-            <div class="mdl-card__supporting-text">
-                <?php foreach ($post_tags as $tag) {
-                    printf('<a href="%1$s" class="tag-chip"><span class="mdl-chip"><span class="mdl-chip__text">%2$s</span></span></a>',
-                        get_term_link($tag), $tag->name);
-                } ?>
-            </div>
-        <?php }; ?>
-        <div class="mdl-card__supporting-text entry-content mdl-color-text--grey-800">
-            <?php is_single() ? the_content() : the_excerpt();
-            ?>
-        </div><!-- .entry-content -->
-        <span class="flex-padding"></span>
-        <div class="mdl-card__actions entry-footer mdl-grid">
-            <?php
-            $post_cat = get_the_category()[0];
-            printf('<a href="%1$s" class="mdl-button md-js-button md-js-ripple-effect"><i class="material-icons">label</i>%2$s</a>',
-                get_term_link($post_cat), $post_cat->name);
-
-            printf('<a href ="%1$s" class="mdl-button md-js-button md-js-ripple-effect" rel="bookmark">
-                    <i class="material-icons">update</i>
-                    <time class="entry-date published updated" datetime="%2$s">%3$s</time></a>',
-                esc_url(get_permalink()), esc_attr(get_the_date('c')), esc_html(get_the_date()));
-            printf('<a href="%1$s" class="mdl-button md-js-button md-js-ripple-effect"><i class="material-icons">comment</i>%2$s comment</a>',
-                get_permalink() . '#comments', get_comments_number());
-            ?>
-            <?php if (!is_single()): ?>
-                <span class="flex-padding"></span>
-                <?php
-                printf('<a href="%1$s" class="mdl-button md-js-button md-js-ripple-effect">read more<i class="material-icons">expand_more</i></a>',
-                    get_permalink());
-                ?>
-            <?php endif; ?>
-        </div><!-- .entry-footer -->
-    </div>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'mdl-grid mdl-grid--no-spacing mdl-shadow--2dp' ); ?>>
+	<?php if ( ! is_single() ): ?>
+		<header class="">
+			<?php the_post_thumbnail( 'w320h320' ); ?>
+		</header>
+	<?php endif; ?>
+	<div class="mdl-card">
+		<div class="mdl-card__title entry-header">
+			<?php the_title( '<h2 class="mdl-card__title-text entry-title">', '</h2>' ) ?>
+		</div>
+		<?php $post_tags = get_the_tags();
+		if ( $post_tags ) : ?>
+			<div class="mdl-card__supporting-text">
+				<?php foreach ( $post_tags as $tag ) : ?>
+					<a href="<?php echo get_term_link( $tag ); ?>" class="tag-chip">
+				<span class="mdl-chip">
+					<span class="mdl-chip__text"><?php echo $tag->name; ?></span>
+				</span>
+					</a>
+				<?php endforeach; ?>
+			</div>
+		<?php endif; ?>
+		<div class="mdl-card__supporting-text entry-content mdl-color-text--grey-800">
+			<?php is_single() ? the_content() : the_excerpt(); ?>
+		</div><!-- .entry-content -->
+		<span class="flex-padding"></span>
+		<div class="mdl-card__actions entry-footer mdl-grid">
+			<?php
+			$post_cat = get_the_category()[0];
+			?>
+			<a href="<?php echo get_term_link( $post_cat ); ?>" class="mdl-button md-js-button md-js-ripple-effect">
+				<i class="material-icons">label</i><?php echo $post_cat->name; ?>
+			</a>
+			<a href="<?php echo esc_url( get_permalink() ); ?>" class="mdl-button md-js-button md-js-ripple-effect"
+			   rel="bookmark">
+				<i class="material-icons">update</i>
+				<time class="entry-date published updated" datetime="<?php echo esc_attr( get_the_date( 'c' ) ); ?>">
+					<?php echo esc_html( get_the_date() ) ?>
+				</time>
+			</a>
+			<a href="<?php echo get_permalink() . '#comments'; ?>" class="mdl-button md-js-button md-js-ripple-effect">
+				<i class="material-icons">comment</i><?php echo get_comments_number(); ?> comment
+			</a>
+			<?php if ( ! is_single() ): ?>
+				<span class="flex-padding"></span>
+				<a href="<?php get_permalink(); ?>" class="mdl-button md-js-button md-js-ripple-effect">
+					read more<i class="material-icons">expand_more</i>
+				</a>
+			<?php endif; ?>
+		</div><!-- .entry-footer -->
+	</div>
 </article><!-- #post-## -->
