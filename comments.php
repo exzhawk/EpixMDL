53a869eb05b1091ac6a2c7b0d60d4d5f2b5e292b
Epix <git@exz.me>
2016-10-10 21:53:23 +0800

    usable comment

--- comments.php
+++ comments.php
@@ -40,30 +40,48 @@ if ( post_password_required() ) {
 			?>
 		</ul>
 	<?php endif; // Check for have_comments().
-	comment_form();
 	?>
 	<div id="respond" class="mdl-card__supporting-text">
 		<form action="<?php echo esc_url( site_url( '/wp-comments-post.php' ) ); ?>" method="post" id="commentform"
-		      class="comment-form">
-			<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label comment-form-author">
-				<input class="mdl-textfield__input" type="text" id="author">
-				<label class="mdl-textfield__label" for="author">Name</label>
+		      class="comment-form mdl-grid">
+			<div class="comment-text-wrapper mdl-cell mdl-cell--4-col">
+				<label class="mdl-button mdl-js-button mdl-button--icon" for="author">
+					<i class="material-icons">person</i>
+				</label>
+				<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label comment-form-author">
+					<input class="mdl-textfield__input" type="text" id="author" name="author" required>
+					<label class="mdl-textfield__label" for="author">Name</label>
+				</div>
 			</div>
-			<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label comment-form-email">
-				<input class="mdl-textfield__input" type="text" id="email">
-				<label class="mdl-textfield__label" for="email">Email</label>
+			<div class="comment-text-wrapper mdl-cell mdl-cell--4-col">
+				<label class="mdl-button mdl-js-button mdl-button--icon" for="email">
+					<i class="material-icons">email</i>
+				</label>
+				<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label comment-form-email">
+					<input class="mdl-textfield__input" type="email" id="email" name="email" required>
+					<label class="mdl-textfield__label" for="email">Email</label>
+				</div>
 			</div>
-			<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label comment-form-url">
-				<input class="mdl-textfield__input" type="text" id="url">
-				<label class="mdl-textfield__label" for="url">Website</label>
+			<div class="comment-text-wrapper mdl-cell mdl-cell--4-col">
+				<label class="mdl-button mdl-js-button mdl-button--icon" for="url">
+					<i class="material-icons">link</i>
+				</label>
+				<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label comment-form-url">
+					<input class="mdl-textfield__input" type="text" id="url" name="url">
+					<label class="mdl-textfield__label" for="url">Website</label>
+				</div>
 			</div>
-			<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label comment-form-comment">
-				<textarea class="mdl-textfield__input" rows= "3" id="comment" ></textarea>
-				<label class="mdl-textfield__label" for="comment">Content</label>
+			<div class="comment-text-wrapper mdl-cell mdl-cell--12-col">
+				<label class="mdl-button mdl-js-button mdl-button--icon" for="url">
+					<i class="material-icons">link</i>
+				</label>
+				<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label comment-form-comment">
+					<textarea class="mdl-textfield__input" rows="3" id="comment" name="comment" required></textarea>
+					<label class="mdl-textfield__label" for="comment">Content</label>
+				</div>
 			</div>
-			<button name="submit" type="submit" id="submit" class="mdl-button mdl-js-button mdl-button--primary submit">
-				submit
-			</button>
+			<input name="submit" type="submit" id="submit" class="mdl-button mdl-js-button mdl-button--primary submit">
+			<?php echo get_comment_id_fields(); ?>
 		</form>
 	</div>
 </div><!-- #comments -->
